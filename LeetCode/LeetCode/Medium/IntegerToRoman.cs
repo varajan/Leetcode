a4a5f870bc6c32d3f0f5da127c9bfecd384b4b14
Sergii Dmytrenko <s.dmytrenko@evi9.com>
2018-09-26 18:55:06 +0300

    #5 Integer to Roman

---
+++ LeetCode/LeetCode/Medium/IntegerToRoman.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Medium
+{
+    public class IntegerToRoman
+    {
+        public string IntToRoman(int num)
+        {
+            var replaces = new List<List<string>>
+            {
+                new List<string> {"DCCCC", "CM"},
+                new List<string> {"CCCC", "CD"},
+                new List<string> {"LXXXX", "XC"},
+                new List<string> {"XXXX", "XL"},
+                new List<string> {"VIIII", "IX"},
+                new List<string> {"IIII", "IV"}
+            };
+
+            var result = "";
+
+            foreach (var x in new [] {'M', 'D', 'C', 'L', 'X', 'V', 'I'})
+            {
+                while (num >= GetInt(x))
+                {
+                    result += x;
+                    num -= GetInt(x);
+                }
+            }
+
+            foreach (var replace in replaces)
+            {
+                result = result.Replace(replace.First(), replace.Last());
+            }
+
+            return result;
+        }
+
+        private static int GetInt(char roman)
+        {
+            var result = 0;
+
+            switch (roman)
+            {
+                case 'I':
+                    result = 1;
+                    break;
+                case 'V':
+                    result = 5;
+                    break;
+                case 'X':
+                    result = 10;
+                    break;
+                case 'L':
+                    result = 50;
+                    break;
+                case 'C':
+                    result = 100;
+                    break;
+                case 'D':
+                    result = 500;
+                    break;
+                case 'M':
+                    result = 1000;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/IntegerToRoman.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class IntegerToRoman
+    {
+        private static LeetCode.Medium.IntegerToRoman Roman => new LeetCode.Medium.IntegerToRoman();
+
+        [TestMethod]
+        public void Test1_Empty_is_0() => Assert.AreEqual(string.Empty, Roman.IntToRoman(0));
+
+        [TestMethod]
+        public void Test2_III_is_3() => Assert.AreEqual("III", Roman.IntToRoman(3));
+
+        [TestMethod]
+        public void Test3_IV_is_4() => Assert.AreEqual("IV", Roman.IntToRoman(4));
+
+        [TestMethod]
+        public void Test4_IX_is_9() => Assert.AreEqual("IX", Roman.IntToRoman(9));
+
+        [TestMethod]
+        public void Test5_LVIII_is_58() => Assert.AreEqual("LVIII", Roman.IntToRoman(58));
+
+        [TestMethod]
+        public void Test6_MCMXCIV_is_1994() => Assert.AreEqual("MCMXCIV", Roman.IntToRoman(1994));
+
+        [TestMethod]
+        public void Test7_MMCDLXXIV_is_2474() => Assert.AreEqual("MMCDLXXIV", Roman.IntToRoman(2474));
+   }
+}
