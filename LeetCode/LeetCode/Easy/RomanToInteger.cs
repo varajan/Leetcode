420cd33b6b3133298aa3f39e31e6fff413b17d3d
Sergii Dmytrenko <s.dmytrenko@evi9.com>
2018-09-26 13:11:58 +0300

    #4 Romant To Integer. Code + unit tests

---
+++ LeetCode/LeetCode/Easy/RomanToInteger.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+
+namespace LeetCode.Easy
+{
+    public class RomanToInteger
+    {
+        public int RomanToInt(string s)
+        {
+            return s.Select((t, i) => (i + 1 < s.Length && GetInt(t) < GetInt(s[i + 1]) ? -GetInt(t) : GetInt(t))).Sum();
+        }
+
+        private static int GetInt(char roman)
+        {
+            var result = 0;
+
+            switch (roman)
+            {
+                case 'I': result = 1;
+                break;
+                case 'V': result = 5;
+                break;
+                case 'X': result = 10;
+                break;
+                case 'L': result = 50;
+                break;
+                case 'C': result = 100;
+                break;
+                case 'D': result = 500;
+                break;
+                case 'M': result = 1000;
+                break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Easy/RomanToInteger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Easy
+{
+    [TestClass]
+    public class RomanToInteger
+    {
+        private static LeetCode.Easy.RomanToInteger Roman => new LeetCode.Easy.RomanToInteger();
+
+        [TestMethod]
+        public void Test1_Empty_is_0() => Assert.AreEqual(0, Roman.RomanToInt(string.Empty));
+
+        [TestMethod]
+        public void Test2_III_is_3() => Assert.AreEqual(3, Roman.RomanToInt("III"));
+
+        [TestMethod]
+        public void Test3_IV_is_4() => Assert.AreEqual(4, Roman.RomanToInt("IV"));
+
+        [TestMethod]
+        public void Test4_IX_is_9() => Assert.AreEqual(9, Roman.RomanToInt("IX"));
+
+        [TestMethod]
+        public void Test5_LVIII_is_58() => Assert.AreEqual(58, Roman.RomanToInt("LVIII"));
+
+        [TestMethod]
+        public void Test6_MCMXCIV_is_1994() => Assert.AreEqual(1994, Roman.RomanToInt("MCMXCIV"));
+
+        [TestMethod]
+        public void Test7_MMCDLXXIV_is_2474() => Assert.AreEqual(2474, Roman.RomanToInt("MMCDLXXIV"));
+   }
+}
