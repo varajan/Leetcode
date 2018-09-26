5e71d13e9d68e9d60a09e54c0e160d2f0170eccd
Sergii Dmytrenko <s.dmytrenko@evi9.com>
2018-09-26 09:58:39 +0300

    #3 ReverseInteger - code + unit tests

---
+++ LeetCode/LeetCode/Easy/ReverseInteger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Easy
+{
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            if (x == int.MinValue) return 0;
+            var positive = x > 0;
+            var asString = Math.Abs(x).ToString().Aggregate("", (current, c) => c + current);
+
+            return int.TryParse(asString, out var result) ? (positive ? result : -result) : 0;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Easy/ReverseInteger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Easy
+{
+    [TestClass]
+    public class ReverseInteger
+    {
+        private static LeetCode.Easy.ReverseInteger Reverse => new LeetCode.Easy.ReverseInteger();
+
+        [TestMethod]
+        public void Test1_Positive_number() => Assert.AreEqual(321, Reverse.Reverse(123));
+
+        [TestMethod]
+        public void Test2_Negative_number() => Assert.AreEqual(-321, Reverse.Reverse(-123));
+
+        [TestMethod]
+        public void Test3_Number_ends_with_zeros() => Assert.AreEqual(321, Reverse.Reverse(12300));
+
+        [TestMethod]
+        public void Test4_Number_contains_zeros() => Assert.AreEqual(30201, Reverse.Reverse(10203));
+
+        [TestMethod]
+        public void Test5_Result_overflows_for_positive_number() => Assert.AreEqual(0, Reverse.Reverse(int.MaxValue));
+
+        [TestMethod]
+        public void Test5_Result_overflows_for_negative_number() => Assert.AreEqual(0, Reverse.Reverse(int.MinValue));
+   }
+}
