0299652db65850ed27104dc3fe5577d3ed47e0ef
Sergii Dmytrenko <s.dmytrenko@evi9.com>
2018-09-27 14:20:25 +0300

    #6 Validate Palindrom II

--- LeetCode/LeetCode/Easy/Palindrome.cs
+++ LeetCode/LeetCode/Easy/Palindrome.cs
@@ -4,6 +4,22 @@
     {
         public static bool Validate2(string s)
         {
+            if (string.IsNullOrEmpty(s)) return false;
+
+            var step = 0;
+            var length = s.Length;
+
+            for (var i = 0; i < length / 2; i++)
+            {
+                if (s[i + step] != s[length - i - 1] && s[i] != s[length - step - i - 1])
+                {
+                    step++;
+                    i--;
+                }
+
+                if (step > 1) return false;
+            }
+
             return true;
         }
     }
---
+++ LeetCode/LeetCodeTests/Easy/Palindrome.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindromer = LeetCode.Easy.Palindrome;
+
+namespace LeetCodeTests.Easy
+{
+    [TestClass]
+    public class Palindrome
+    {
+        [TestMethod]
+        public void Test01_Nothing_to_remove_to_get_palindrome() => Assert.AreEqual(true, Palindromer.Validate2("abcba"));
+
+        [TestMethod]
+        public void Test02_One_char_to_remove_to_get_palindrome() => Assert.AreEqual(true, Palindromer.Validate2("abcbba"));
+
+        [TestMethod]
+        public void Test03_Two_chars_to_remove_to_get_palindrome() => Assert.AreEqual(false, Palindromer.Validate2("abdacbba"));
+
+        [TestMethod]
+        public void Test04_Empty_string_is_not_a_palindrome() => Assert.AreEqual(false, Palindromer.Validate2(string.Empty));
+
+        [TestMethod]
+        public void Test05_Not_a_palindrome() => Assert.AreEqual(false, Palindromer.Validate2("abcdefghjklmn"));
+    }
+}
