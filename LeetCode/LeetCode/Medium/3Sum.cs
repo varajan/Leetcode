b03a55ef93b33b26814846b3dc257c0950438f40
Sergii Dmytrenko <sergii.dmytrenko@exact.com>
2019-02-08 11:02:16 +0200

    Performance test: 12.5 seconds

--- LeetCode/LeetCode/Medium/3Sum.cs
+++ LeetCode/LeetCode/Medium/3Sum.cs
@@ -3,42 +3,48 @@ using System.Linq;
 
 namespace LeetCode.Medium
 {
-	public static class _3Sum
-	{
-		public static List<List<int>> ThreeSum(int[] nums)
-		{
-			var result = new List<List<int>>();
-			var set = new HashSet<string>();
+    public static class _3Sum
+    {
+        public static List<List<int>> ThreeSum(int[] nums)
+        {
+            var result = new List<List<int>>();
+            var set = new HashSet<string>();
+            var jj = false;
 
-			if (nums.Count() < 3)
-			{
-				return result;
-			}
+            if (nums.Length < 3)
+            {
+                return result;
+            }
 
-			nums = nums.OrderBy(x => x).ToArray();
+            nums = nums.OrderBy(x => x).ToArray();
 
-			for (var i = 0; i < nums.Count() - 2; i++)
-			{
-				for (var j = i+1; j < nums.Count() - 1; j++)
-				{
-					for (var k = j+1; k < nums.Count(); k++)
-					{
-						if (nums[i] + nums[j] + nums[k] == 0)
-						{
-							var row = new List<int> {nums[i], nums[j], nums[k]}.OrderBy(x => x).ToList();
-							var line = $"{nums[i]},{nums[j]},{nums[k]}";
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-							if (!set.Contains(line))
-							{
-								result.Add(row);
-								set.Add(line);
-							}
-						}
-					}
-				}
-			}
+                jj = false;
+                for (var j = nums.Length - 1; j > i; j--)
+                {
+                    if (jj && nums[j] == nums[j + 1]) continue;
+                    jj = true;
 
-			return result;
-		}
-	}
+                    for (var k = i + 1; k < j; k++)
+                    {
+                        if (nums[i] + nums[j] + nums[k] == 0)
+                        {
+                            var line = $"{nums[i]},{nums[j]},{nums[k]}";
+
+                            if (!set.Contains(line))
+                            {
+                                result.Add(new List<int> { nums[i], nums[j], nums[k] });
+                                set.Add(line);
+                            }
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
 }
