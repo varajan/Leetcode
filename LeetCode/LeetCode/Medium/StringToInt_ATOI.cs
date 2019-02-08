add9a9c918cfcaa15c4bbd3e55ec0d693a58001b
Sergii Dmytrenko <sergii.dmytrenko@exact.com>
2019-02-08 15:56:29 +0200

    #9 Simple solution

---
+++ LeetCode/LeetCode/Medium/StringToInt_ATOI.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCode.Medium
+{
+    public static class StringToInt_ATOI
+    {
+        public static int MyAtoi(string str)
+        {
+            str = str.Trim() + ".";
+            bool negative = (str[0] == '-');
+
+            var number = "0";
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                if (i == 0 && (str[0] == '-' || str[0] == '+'))
+                {
+                    continue;
+                }
+
+                if (char.IsDigit(str[i]))
+                {
+                    number += str[i];
+                }
+                else break;
+            }
+
+            var result = double.Parse(number) * (negative ? -1 : 1);
+
+            if (result > int.MaxValue) return int.MaxValue;
+            if (result < int.MinValue) return int.MinValue;
+
+            return (int)result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/StringToInt_ATOI.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using String2Int = LeetCode.Medium.StringToInt_ATOI;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class StringToInt_ATOI
+    {
+        [TestMethod]
+        public void StringToInt_ATOI_01_asdf_123_is_0() => Assert.AreEqual(0, String2Int.MyAtoi("asdf 123"));
+
+        [TestMethod]
+        public void StringToInt_ATOI_02_42_is_42() => Assert.AreEqual(42, String2Int.MyAtoi("42"));
+
+        [TestMethod]
+        public void StringToInt_ATOI_03_minus_42_is_minus_42() => Assert.AreEqual(-42, String2Int.MyAtoi("-42"));
+
+        [TestMethod]
+        public void StringToInt_ATOI_04_2147483648_is_2147483647() => Assert.AreEqual(2147483647, String2Int.MyAtoi("2147483648"));
+
+        [TestMethod]
+        public void StringToInt_ATOI_05_minus_2147483650_is_minus_2147483648() => Assert.AreEqual(-2147483648, String2Int.MyAtoi("-2147483650"));
+    }
+}
