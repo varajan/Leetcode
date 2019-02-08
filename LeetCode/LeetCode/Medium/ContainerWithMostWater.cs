fa5498bc24ca49e354d527f228b470476c1a9ca0
Sergii Dmytrenko <sergii.dmytrenko@exact.com>
2019-02-08 15:10:02 +0200

    #8 Simple sollution

---
+++ LeetCode/LeetCode/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Medium
+{
+    public static class ContainerWithMostWater
+    {
+        public static int MaxArea(int[] height)
+        {
+            var result = 0;
+
+            if (height.Length < 2) return result;
+
+            for (var i = 0; i < height.Length-1; i++)
+            {
+                if (height[i]==0) continue;
+
+                for (var j = height.Length-1; j > i; j--)
+                {
+                    if (height[j] == 0) continue;
+
+                    var s = (j-i) * Math.Min(height[i], height[j]);
+                    result = Math.Max(result, s);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/ContainerWithMostWater.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Container = LeetCode.Medium.ContainerWithMostWater;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class ContainerWithMostWater
+    {
+        [TestMethod]
+        public void ContainerWithMostWater_01() => Assert.AreEqual(0, Container.MaxArea(new int[]{}));
+
+        [TestMethod]
+        public void ContainerWithMostWater_02() => Assert.AreEqual(49, Container.MaxArea(new int[] {1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+}
