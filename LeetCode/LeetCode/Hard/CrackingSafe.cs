24fa754f8b8bf0f67771f0cb37008f429ef361d0
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-02-14 09:06:50 +0200

    BrutForce way

---
+++ LeetCode/LeetCode/Hard/CrackingSafe.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LeetCode.Hard
+{
+    public static class CrackingSafe
+    {
+        public static string CrackSafe(int n, int k)
+        {
+            // n - length
+            var codes = k.GenerateAllCombinations(n);
+
+            var result = string.Empty;
+
+            while (codes.Count > 0)
+            {
+                //var indexes = new List<int>();
+                //for (var i = 0; i < codes.Count; i++)
+                //{
+                //    if (result.Contains(codes[i]))
+                //    {
+                //        indexes.Add(i);
+                //    }
+                //}
+
+                //indexes
+                //    .OrderByDescending(index => index).ToList()
+                //    .ForEach(index => codes.RemoveAt(index));
+
+                foreach (var code in codes)
+                {
+                    var found = false;
+
+                    if (result.Contains(code))
+                    {
+                        codes.Remove(code);
+                        break;
+                    }
+
+                    for (var length = n-1; length > 0; length--)
+                    {
+                        if (result.StartsWith(code.Substring(length)))
+                        {
+                            result = code.Substring(0, length) + result;
+                            codes.Remove(code);
+                            found = true;
+                            break;
+                        }
+
+                        if (result.EndsWith(code.Substring(0, length)))
+                        {
+                            result += code.Substring(length);
+                            codes.Remove(code);
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (found) break;
+
+                    if (codes.Any())
+                    {
+                        result += codes.First();
+                        codes.Remove(codes.First());
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Hard/CrackingSafe.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Crack = LeetCode.Hard.CrackingSafe;
+
+namespace LeetCodeTests.Hard
+{
+    [TestClass]
+    public class CrackingSafe
+    {
+        [TestMethod]
+        public void CrackingSafe_01() => Assert.AreEqual("01", Crack.CrackSafe(1, 2));
+
+        [TestMethod]
+        public void CrackingSafe_02() => Assert.AreEqual("00110", Crack.CrackSafe(2, 2));
+
+        [TestMethod]
+        public void CrackingSafe_03() => Assert.AreEqual("0011101000", Crack.CrackSafe(3, 2));
+    }
+}
