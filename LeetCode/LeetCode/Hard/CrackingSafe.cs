9a836eeb77b1206fa3e1cb6e775b1dd22ab4b06e
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-02-18 14:29:20 +0200

    Now test 2from4 takes 7 seconds

--- LeetCode/LeetCode/Hard/CrackingSafe.cs
+++ LeetCode/LeetCode/Hard/CrackingSafe.cs
@@ -12,25 +12,12 @@ namespace LeetCode.Hard
             var length = k.Power(n) + (n - 1);
             var codes = k.GenerateAllCombinations(n);
 
-            //var combinations = new List<string>();
-
-            //foreach (var key in Keys(length, k))
-            //{
-            //    var combination = "";
-            //    foreach (var i in key)
-            //    {
-            //        combination += i;
-            //    }
-
-            //    combinations.Add(combination);
-            //}
-
             foreach (var key in Keys(length, k))
             {
                 var combination = "";
                 foreach (var i in key)
                 {
-                    combination += i;
+                    combination += (char)(i+48);
                 }
 
                 if (codes.All(code => combination.Contains(code))) return combination;
@@ -42,10 +29,9 @@ namespace LeetCode.Hard
         private static IEnumerable<List<int>> Keys(double length, int max)
         {
             var result = new List<int>();
-
             for (var i = 0; i < length; i++)
             {
-                result.Add(Math.Min(i, max-1));
+                result.Add(Math.Min(i, max - 1));
             }
 
             yield return result;
@@ -62,6 +48,8 @@ namespace LeetCode.Hard
 
             for (int i = row.Count - 1; i >= 0; i--)
             {
+                if (overflow == 0) break;
+
                 var next = row[i] + overflow;
                 overflow = next >= max ? 1 : 0;
                 row[i] = next >= max ? 0 : next;
@@ -110,6 +98,57 @@ namespace LeetCode.Hard
         {
             //var codes = k.GenerateAllCombinations(n);
             codes.Reverse();
+            var result = string.Empty;
+ 
+            while (codes.Count > 0)
+            {
+                foreach (var code in codes)
+                {
+                    var found = false;
+
+                    if (result.Contains(code))
+                    {
+                        codes.Remove(code);
+                        break;
+                    }
+
+                    for (var length = n-1; length > 0; length--)
+                    {
+                        if (result.StartsWith(code.Substring(length-1)))
+                        {
+                            result = code.Substring(0, length-1) + result;
+                            codes.Remove(code);
+                            found = true;
+                            break;
+                        }
+
+                        if (result.EndsWith(code.Substring(0, length)))
+                        {
+                            result += code.Substring(length);
+                            codes.Remove(code);
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (found) break;
+
+                    if (codes.Any())
+                    {
+                        result += codes.First();
+                        codes.Remove(codes.First());
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public static string InitialCombination(int n, int k, List<string> codes)
+        {
+            //var codes = k.GenerateAllCombinations(n);
+            //codes.Reverse();
             var result = string.Empty;
  
             while (codes.Count > 0)
