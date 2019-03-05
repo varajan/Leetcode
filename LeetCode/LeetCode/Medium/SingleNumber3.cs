07cbb607e78fc33f0303b879360977812f33abdb
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-03-05 12:26:44 +0200

    #12 Fast sollution.

---
+++ LeetCode/LeetCode/Medium/SingleNumber3.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Medium
+{
+    public class SingleNumber3
+    {
+        public int[] SingleNumber(int[] nums)
+        {
+            var dictionary = new Dictionary<int, int>();
+
+            foreach (int num in nums)
+            {
+                dictionary[num] = dictionary.ContainsKey(num) ? dictionary[num] + 1 : 1;
+            }
+
+            return dictionary.Where(x => x.Value == 1).Select(x => x.Key).ToArray();
+        }
+    }
+}
