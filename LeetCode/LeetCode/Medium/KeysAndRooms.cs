9ba4ae24d5195e4e7d33e78d9256232214e281e4
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-03-05 11:00:39 +0200

    #11 Draft solution.

---
+++ LeetCode/LeetCode/Medium/KeysAndRooms.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Medium
+{
+    public static class KeysAndRooms
+    {
+        public static bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            var visited = new bool[rooms.Count].ToList();
+            if (rooms.Count > 1 && !rooms.First().Any()) return false;
+
+            VisitRoom(0, rooms, ref visited);
+
+            return visited.All(x => x);
+        }
+
+        private static void VisitRoom(int room, IList<IList<int>> rooms, ref List<bool> visited)
+        {
+            visited[room] = true;
+            if (visited.All(x => x)) return;
+
+            var keys = rooms[room];
+            foreach (var key in keys)
+            {
+                if (!visited[key]) VisitRoom(key, rooms, ref visited);
+                visited[key] = true;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/KeysAndRooms.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Keys = LeetCode.Medium.KeysAndRooms;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class KeysAndRooms
+    {
+        [TestMethod]
+        public void KeysAndRooms_01()
+        {
+            var rooms = new List<IList<int>>
+            {
+                new List<int> {1},
+                new List<int> {2},
+                new List<int> {3},
+                new List<int> {}
+            };
+
+            Assert.IsTrue(Keys.CanVisitAllRooms(rooms));
+        }
+
+        [TestMethod]
+        public void KeysAndRooms_02()
+        {
+            var rooms = new List<IList<int>>
+            {
+                new List<int> {1, 3},
+                new List<int> {3, 0, 1},
+                new List<int> {2},
+                new List<int> {0}
+            };
+
+            Assert.IsFalse(Keys.CanVisitAllRooms(rooms));
+        }
+    }
+}
