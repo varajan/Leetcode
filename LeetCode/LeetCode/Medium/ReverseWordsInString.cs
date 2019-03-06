d032685679bac766f02ca74b5f7bbea73946ace1
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-03-06 13:22:36 +0200

    #13 Reverse words;

---
+++ LeetCode/LeetCode/Medium/ReverseWordsInString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    class ReverseWordsInString
+    {
+        public string ReverseWords(string s)
+        {
+            var words = s.Split(' ').Where(x => x.Trim().Length > 0).Reverse();
+
+            var result = new StringBuilder();
+            foreach (var word in words)
+            {
+                result.Append(word);
+                result.Append(" ");
+            }
+
+            return result.ToString().Trim();
+        }
+    }
+}
