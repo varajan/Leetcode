f7aad58078bbe2f07d82e62d84fb237cbdbb9193
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-03-06 15:23:01 +0200

    #14 Validate IP using regex and linq.

---
+++ LeetCode/LeetCode/Medium/ValidateIpAddress.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Medium
+{
+    public static class ValidateIpAddress
+    {
+        public static string ValidateAddress(string IP)
+        {
+            var v4 = "[0-9]{1,3}";
+            var v6 = "[0-9a-fA-F]{1,4}";
+            var ipv4 = $"^{v4}.{v4}.{v4}.{v4}$";
+            var ipv6 = $"^{v6}:{v6}:{v6}:{v6}:{v6}:{v6}:{v6}:{v6}$";
+
+            if (Regex.IsMatch(IP, ipv6)) return "IPv6";
+            if (Regex.IsMatch(IP, ipv4)
+                && IP.Split('.').All(x => x == "0" || !x.StartsWith("0") && int.Parse(x) < 256))
+                return "IPv4";
+
+            return "Neither";
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/ValidateIpAddress.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Validator = LeetCode.Medium.ValidateIpAddress;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class ValidateIpAddress
+    {
+        [TestMethod]
+        public void ValidateIpAddress_01() =>
+            Assert.AreEqual("IPv4", Validator.ValidateAddress("172.16.254.1"));
+
+        [TestMethod]
+        public void ValidateIpAddress_02() =>
+            Assert.AreEqual("Neither", Validator.ValidateAddress("172.516.254.1"));
+
+        [TestMethod]
+        public void ValidateIpAddress_03() =>
+            Assert.AreEqual("Neither", Validator.ValidateAddress("1e1.4.5.6"));
+
+        [TestMethod]
+        public void ValidateIpAddress_04() =>
+            Assert.AreEqual("IPv6", Validator.ValidateAddress("2001:0db8:85a3:0:0:8A2E:0370:7334"));
+
+        [TestMethod]
+        public void ValidateIpAddress_05() =>
+            Assert.AreEqual("Neither", Validator.ValidateAddress("01.01.01.01"));
+
+        [TestMethod]
+        public void ValidateIpAddress_06() =>
+            Assert.AreEqual("Neither", Validator.ValidateAddress("20EE:FGb8:85a3:0:0:8A2E:0370:7334"));
+
+        [TestMethod]
+        public void ValidateIpAddress_07() =>
+            Assert.AreEqual("IPv4", Validator.ValidateAddress("192.0.0.1"));
+
+        [TestMethod]
+        public void ValidateIpAddress_08() =>
+            Assert.AreEqual("Neither", Validator.ValidateAddress("00.0.0.0"));
+    }
+}
