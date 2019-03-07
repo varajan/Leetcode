df6400af96f3c8fd96110d2ef38dc3148f5d91db
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-03-07 13:30:25 +0200

    Works for some cases, but too complicated.

---
+++ LeetCode/LeetCode/Medium/DecodedStringAtIndex.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LeetCode.Medium
+{
+    public static class DecodedStringAtIndex
+    {
+        public static string DecodeAtIndex(string S, int K)
+        {
+            double count;
+            var regex = "([a-z]+)(\\d*)";
+            var parts = Regex.Matches(S, regex).Select(x => x.Value);
+            var segments = parts.Select(part =>
+                new Tuple<string, double>(Regex.Match(part, regex).Groups[1].Value,
+                    double.TryParse(Regex.Match(part, regex).Groups[2].Value, out count) ? count : 1)).ToList();
+
+            double length = 0;
+            var lengths = new List<double>();
+            for (var i = 0; i < segments.Count; i++)
+            {
+                length += segments[i].Item1.Length;
+                lengths.Add(length);
+                length *= segments[i].Item2;
+
+                if (K <= length) break;
+            }
+
+            var ii = K % lengths.Last() == 0 ? lengths.Last() : K % lengths.Last();
+            int subSequence = 0;
+            for (; subSequence < lengths.Count; subSequence++)
+            {
+                if (ii <= lengths[subSequence]) break;
+            }
+
+            double index = 0;
+            if (subSequence == 0)
+            {
+                index = ii > 0 ? ii : Math.Min(K, lengths[subSequence]);
+            }
+            else
+            {
+                index = ii - lengths[subSequence - 1] * segments[subSequence - 1].Item2;
+
+                if (index <= 0)
+                {
+                    subSequence--;
+                    
+                    index = ii % lengths[subSequence];
+                    if (index == 0) index = lengths[subSequence];
+                }
+            }
+
+            return segments[subSequence].Item1.Length > (int) index - 1
+                ? segments[subSequence].Item1[(int) index - 1].ToString()
+                : DecodeAtIndex(S, (int) index);
+        }
+//         public static string DecodeAtIndex(string S, int K)
+//        {
+//            double count;
+//            var regex = "([a-z]+)(\\d*)";
+//            var parts = Regex.Matches(S, regex).Select(x => x.Value);
+//            var segments = parts.Select(part =>
+//                new Tuple<string, double>(Regex.Match(part, regex).Groups[1].Value,
+//                    double.TryParse(Regex.Match(part, regex).Groups[2].Value, out count) ? count : 1)).ToList();
+//
+//            double length = 0;
+//            var lengths = new List<double>();
+//            for (var i = 0; i < segments.Count; i++)
+//            {
+//                length += segments[i].Item1.Length;
+//                lengths.Add(length);
+//                length *= segments[i].Item2;
+//
+//                if (K <= length) break;
+//            }
+//
+//            var ii = K % lengths.Last() == 0 ? lengths.Last() : K % lengths.Last();
+//            int subSequence = 0;
+//            for (; subSequence < lengths.Count; subSequence++)
+//            {
+//                if (ii <= lengths[subSequence]) break;
+//            }
+//
+//            double index = 0;
+//            if (subSequence == 0)
+//            {
+//                index = ii > 0 ? ii : Math.Min(K, lengths[subSequence]);
+//            }
+//            else
+//            {
+//                index = ii - lengths[subSequence - 1] * segments[subSequence - 1].Item2;
+//
+//                if (index < 0)
+//                {
+//                    subSequence--;
+//                    
+//                    index = ii % lengths[subSequence];
+//                    if (index == 0) index = lengths[subSequence];
+//                }
+//            }
+//
+//            return segments[subSequence].Item1[(int) index - 1].ToString();
+//        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/DecodedStringAtIndex.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Decoder = LeetCode.Medium.DecodedStringAtIndex;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class DecodedStringAtIndex
+    {
+        [TestMethod]
+        public void DecodedStringAtIndex_01() =>
+            Assert.AreEqual("o", Decoder.DecodeAtIndex("leet2code3", 10));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_02() =>
+            Assert.AreEqual("e", Decoder.DecodeAtIndex("leet2code3", 15));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_03() =>
+            Assert.AreEqual("h", Decoder.DecodeAtIndex("ha22", 5));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_04() =>
+            Assert.AreEqual("a", Decoder.DecodeAtIndex("a2345678999999999999999", 1));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_05() =>
+            Assert.AreEqual("a", Decoder.DecodeAtIndex("abc", 1));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_06() =>
+            Assert.AreEqual("a", Decoder.DecodeAtIndex("a23", 6));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_07() =>
+            Assert.AreEqual("k", Decoder.DecodeAtIndex("vk6u5xhq9v", 554));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_08() =>
+            Assert.AreEqual("b", Decoder.DecodeAtIndex("a2b3c4d5e6f7g8h9", 3));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_09() =>
+            Assert.AreEqual("x", Decoder.DecodeAtIndex("ixm5xmgo78", 711));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_10() =>
+            Assert.AreEqual("q", Decoder.DecodeAtIndex("l3mtm5weq7ki78c7hck4", 1933));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_11() =>
+            Assert.AreEqual("q", Decoder.DecodeAtIndex("l3mtm5weq7ki78c7hck4", 165511));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_12() =>
+            Assert.AreEqual("z", Decoder.DecodeAtIndex("vzpp636m8y", 2920));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_13() =>
+            Assert.AreEqual("a", Decoder.DecodeAtIndex("a4vsb832i3zxouzx6vu6", 31179));
+
+        [TestMethod]
+        public void DecodedStringAtIndex_14() =>
+            Assert.AreEqual("d", Decoder.DecodeAtIndex("udq827yx7rxjfa4s63h6", 300563));
+    }
+}
