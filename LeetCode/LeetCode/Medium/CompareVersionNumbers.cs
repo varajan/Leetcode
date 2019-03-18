0c0d0dfc806ed7ef2953a9abbe97615bd07e4e86
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-03-18 16:37:19 +0200

    #16 Compare versions.

---
+++ LeetCode/LeetCode/Medium/CompareVersionNumbers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LeetCode.Medium
+{
+    public static class CompareVersionNumbers
+    {
+        public static int CompareVersion(string version1, string version2)
+        {
+            var v1 = version1.Split('.');
+            var v2 = version2.Split('.');
+
+            var vAsInt1 = 1;
+            var vAsInt2 = 1;
+
+            for (int i = 1; i <= Math.Max(v1.Length, v2.Length); i++)
+            {
+                vAsInt1 *= 10;
+                vAsInt2 *= 10;
+
+                if (v1.Length >= i) vAsInt1 += int.Parse(v1[i - 1]);
+                if (v2.Length >= i) vAsInt2 += int.Parse(v2[i - 1]);
+            }
+
+            if (vAsInt1 == vAsInt2) return 0;
+
+            return vAsInt1 > vAsInt2 ? 1 : -1;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/CompareVersionNumbers.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Version = LeetCode.Medium.CompareVersionNumbers;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class CompareVersionNumbers
+    {
+        [TestMethod]
+        public void CompareVersionNumbers__0_1_is_less_then_1_1() =>
+            Assert.AreEqual(-1, Version.CompareVersion("0.1", "1.1"));
+
+        [TestMethod]
+        public void CompareVersionNumbers__1_is_less_then_1_1() =>
+            Assert.AreEqual(-1, Version.CompareVersion("1", "1.1"));
+
+        [TestMethod]
+        public void CompareVersionNumbers__1_0_1_is_greater_then_1() =>
+            Assert.AreEqual(1, Version.CompareVersion("1.0.1", "1"));
+
+        [TestMethod]
+        public void CompareVersionNumbers__1_0_is_equal_to_1() =>
+            Assert.AreEqual(0, Version.CompareVersion("1.0", "1"));
+    }
+}
