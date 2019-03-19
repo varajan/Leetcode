08246d91c000522ea0b8d008e982b182278f8926
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-03-19 09:09:25 +0200

    #17 Good solution.
    Runtime: 36 ms, faster than 100.00% of C# online submissions for Clumsy Factorial.
    Memory Usage: 12.8 MB, less than 100.00% of C# online submissions for Clumsy Factorial.

---
+++ LeetCode/LeetCode/Medium/ClumsyFactorial.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Medium
+{
+    public static class ClumsyFactorial
+    {
+        public static int Clumsy(int N)
+        {
+            var j = 0;
+            var result = 0;
+            var multipl = N;
+            var added = false;
+            for (var i = N - 1; i > 0; i--)
+            {
+                j++;
+
+                if (j % 4 == 1) { multipl *= i; added = false; }
+                if (j % 4 == 2) { multipl /= i; }
+                if (j % 4 == 3) { result += multipl + i; added = true; }
+                if (j % 4 == 0) { multipl = -i; added = false; }
+            }
+
+            result += added ? 0 : multipl;
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/ClumsyFactorial.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Factorial = LeetCode.Medium.ClumsyFactorial;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class ClumsyFactorial
+    {
+        [TestMethod]
+        public void ClumsyFactorial_4_is_7() => Assert.AreEqual(7, Factorial.Clumsy(4));
+
+        [TestMethod]
+        public void ClumsyFactorial_5_is_7() => Assert.AreEqual(7, Factorial.Clumsy(5));
+
+        [TestMethod]
+        public void ClumsyFactorial_10_is_12() => Assert.AreEqual(12, Factorial.Clumsy(10));
+
+        [TestMethod]
+        [Timeout(30)]
+        public void ClumsyFactorial_8464_is_100() => Assert.AreEqual(8465, Factorial.Clumsy(8464));
+    }
+}
