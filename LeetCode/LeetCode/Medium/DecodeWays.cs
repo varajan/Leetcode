5c7e01d7a9b82e332d64801b2e64b69f66c8e77f
Sergii Dmytrenko <Sergii.Dmytrenko@leaseplan.com>
2019-03-21 14:16:33 +0200

    #18 Final & accepted;

--- LeetCode/LeetCode/Medium/DecodeWays.cs
+++ LeetCode/LeetCode/Medium/DecodeWays.cs
@@ -4,32 +4,27 @@
     {
         public static int NumDecodings(string s)
         {
-            return GetRoots(s, 0);
+            return GetRoots(s);
         }
 
-        private static int GetRoots(string code, int roots)
+        private static int GetRoots(string code, int i = 0, int roots = 0)
         {
-            if (code.Length == 0)
+            if (code.Length == i)
             {
                 return roots;
             }
 
-            bool notNull = code[0] > 48;
-            int roots1 = 0;
+            if (code[i] == 48) return 0;
+
+            int roots1 = GetRoots(code, i + 1, 1);
             int roots2 = 0;
 
-            if (code.Length > 1 && (code[0]==49 || code[0]==50 && code[1] < 55) && notNull)
+            if (code.Length > i+1 && (code[i]==49 || code[i]==50 && code[i+1] < 55))
             {
-                roots1 = GetRoots(code.Substring(2, code.Length - 2), roots1 == 0 ? 1 : roots1);
-            }
-            if (notNull)
-            {
-                roots2 = GetRoots(code.Substring(1, code.Length - 1), roots2 == 0 ? 1 : roots2);
+                roots2 = GetRoots(code, i + 2, 1);
             }
 
             return roots1 + roots2;
         }
-
-        private static int GetCode(string code) => (code[0] - 48) * 10 + code[1] - 48;
     }
 }
