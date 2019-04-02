7f6fbb5ce56b4b026ab4736f67075f4e26545cc9
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-04-02 12:50:41 +0300

    Move indexes out of inner function

--- LeetCode/LeetCode/Hard/DecodeWays2.cs
+++ LeetCode/LeetCode/Hard/DecodeWays2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace LeetCode.Hard
 {
@@ -6,28 +6,33 @@ namespace LeetCode.Hard
     {
         public static int NumDecodings(string s)
         {
-            return GetRoots2(s.ToCharArray());
+            var indexes = new List<short>();
+            short i = -1;
+            foreach (var x in s)
+            {
+                i++;
+                if (x=='*') indexes.Add(i);
+            }
+
+            return GetRoots(s.ToCharArray(), indexes.ToArray());
         }
 
-        private static int GetRoots2(char[] code)
+        private static int GetRoots(char[] code, short[] indexes, int ii = 0)
         {
+            if (ii >= indexes.Length)
+            {
+                return Medium.DecodeWays.GetRoots(code);
+            }
+
             var result = 0;
-            var index = Array.IndexOf(code, '*');
-
-
-            if (index < 0)
+            var next = ii + 1;
+            var index = indexes[ii];
+            for (var i = '1'; i < ':'; i++)
             {
-                result = Medium.DecodeWays.GetRoots(code);
+                code[index] = i;
+                result += GetRoots(code, indexes, next);
             }
-            else
-            {
-                for (var i = '1'; i < ':'; i++)
-                {
-                    code[index] = i;
-                    result += GetRoots2(code);
-                }
-                code[index] = '*';
-            }
+            code[index] = '*';
 
             return result;
         }
--- LeetCode/LeetCodeTests/Hard/DecodeWays.cs
+++ LeetCode/LeetCodeTests/Hard/DecodeWays.cs
@@ -6,24 +6,28 @@ namespace LeetCodeTests.Hard
     [TestClass]
     public class DecodeWays
     {
-        [Timeout(1000)]
+        [Timeout(2000)]
         [TestMethod]
         public void DecodeWays_x_is_9() => Assert.AreEqual(9, Decoder.NumDecodings("*"));
 
-        [Timeout(1000)]
+        [Timeout(2000)]
         [TestMethod]
         public void DecodeWays_1x_is_18() => Assert.AreEqual(18, Decoder.NumDecodings("1*"));
 
-        [Timeout(1000)]
+        [Timeout(2000)]
         [TestMethod]
         public void DecodeWays_xx_is_96() => Assert.AreEqual(96, Decoder.NumDecodings("**"));
 
-        [Timeout(1000)]
+        [Timeout(2000)]
         [TestMethod]
         public void DecodeWays_xxxxxxx_is_11859129() => Assert.AreEqual(11859129, Decoder.NumDecodings("*******"));
 
-        [Timeout(10000)]
+        [Timeout(2000)]
         [TestMethod]
         public void DecodeWays_1xxxxxxx0_is_4232034() => Assert.AreEqual(4232034, Decoder.NumDecodings("1*******0"));
+
+        [Timeout(25000)]
+        [TestMethod]
+        public void DecodeWays_11xxxxxxx0_is_6504516() => Assert.AreEqual(6504516, Decoder.NumDecodings("11*******0"));
     }
 }
