24d74af2ea2417e2468fdcd2aa5c8e6a174dc2f6
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-04-03 14:25:05 +0300

    #19 pretty fast solution

---
+++ LeetCode/LeetCode/Medium/PrimePalindrome.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+namespace LeetCode.Medium
+{
+    public static class PrimePalindrome
+    {
+        public static int GetPrimePalindrome(int N)
+        {
+            if (!IsPalindrome(N)) N = Next(N);
+
+            while (!IsPrime(N))
+            {
+                N = Next(N);
+            }
+
+            return N;
+        }
+
+        private static int Next(int n)
+        {
+            if (All9(n)) return n + 2;
+            if (n < 11) return ++n;
+            var nn = n.ToString();
+            var len = nn.Length;
+
+            return len % 2 == 0 ? NextOdd(n, len) : NextNotOdd(n, len, nn);
+        }
+
+        private static bool All9(int n)
+        {
+            while (n > 0)
+            {
+                if (n % 10 != 9) return false;
+                n /= 10;
+            }
+
+            return true;
+        }
+
+        private static int NextNotOdd(int n, int len, string nn)
+        {
+            var half = (len - 1) / 2;
+            var pow = (int) Math.Pow(10, half);
+            var begin = len > 1 ? n / pow / 10 : 0;
+            var middle = nn[half] - 47;
+
+            if (middle == 10)
+            {
+                middle = 0;
+                begin++;
+            }
+
+            if (begin > 0)
+            {
+                var result = begin;
+                result *= 10;
+                result += middle;
+                result *= pow;
+                result += Reverse(begin);
+
+                return result;
+            }
+
+            return middle;
+        }
+
+        private static int NextOdd(int n, int len)
+        {
+            var half = len / 2;
+            var pow = (int) Math.Pow(10, half);
+            var begin = n / pow + 1;
+
+            return begin * pow + Reverse(begin);
+        }
+
+        private static int Reverse(int number)
+        {
+            var result = 0;
+
+            while (number > 0)
+            {
+                result *= 10;
+                result += number % 10;
+                number /= 10;
+            }
+
+            return result;
+        }
+
+        static bool IsPrime(int n)
+        {
+            if (n <= 3) return n > 1;
+            else if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+            var i = 5;
+            while (i * i <= n)
+            {
+                if (n % i ==0 || n % (i+2)==0)
+                return false;
+
+                i += 6;
+            }
+            return true;
+        }
+
+        private static bool IsPalindrome(int n)
+        {
+            var nn = n.ToString();
+            var len = nn.Length;
+            for (int i = 0; i < len / 2; i++)
+                if (nn[i] != nn[len - i - 1]) return false;
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/PrimePalindrome.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome = LeetCode.Medium.PrimePalindrome;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class PrimePalindrome
+    {
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_1_is_2() => Assert.AreEqual(2, Palindrome.GetPrimePalindrome(1));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_2_is_2() => Assert.AreEqual(2, Palindrome.GetPrimePalindrome(2));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_4_is_5() => Assert.AreEqual(5, Palindrome.GetPrimePalindrome(4));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_6_is_7() => Assert.AreEqual(7, Palindrome.GetPrimePalindrome(6));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_8_is_11() => Assert.AreEqual(11, Palindrome.GetPrimePalindrome(8));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_13_is_101() => Assert.AreEqual(101, Palindrome.GetPrimePalindrome(13));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_10_is_11() => Assert.AreEqual(11, Palindrome.GetPrimePalindrome(10));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_9965700_is_9978799() =>
+            Assert.AreEqual(9978799, Palindrome.GetPrimePalindrome(9965700));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_9989900_is_100030001() =>
+            Assert.AreEqual(100030001, Palindrome.GetPrimePalindrome(9989900));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_45887963_is_100030001() =>
+            Assert.AreEqual(100030001, Palindrome.GetPrimePalindrome(45887963));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_100000000_is_100030001() =>
+            Assert.AreEqual(100030001, Palindrome.GetPrimePalindrome(100000000));
+
+        [Timeout(200)]
+        [TestMethod]
+        public void PrimePalindrome_of_31880255_is_100030001() =>
+            Assert.AreEqual(100030001, Palindrome.GetPrimePalindrome(31880255));
+    }
+}
