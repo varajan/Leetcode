f3d059ad13108b9e9dc80eafeb0bab64f14df6c1
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-05-15 14:30:16 +0300

    #26 Multiply strings

---
+++ LeetCode/LeetCode/Medium/MultiplyStrings.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Medium
+{
+    public class MultiplyStrings
+    {
+        private Dictionary<char, string> Memory = new Dictionary<char, string>();
+
+        public string Multiply(string num1, string num2)
+        {
+            string result = "";
+            var i = "";
+
+            if (num1.Length < num2.Length)
+            {
+                var t = num1;
+                num1 = num2;
+                num2 = t;
+            }
+
+            for (int j = num1.Length-1; j >= 0; j--)
+            {
+                string multipy;
+                var key = num1[j];
+                if (Memory.ContainsKey(key))
+                {
+                    multipy = Memory[key];
+                }
+                else
+                {
+                    multipy = Myltiply(num2, num1[j]);
+                    Memory.Add(key, multipy);
+                }
+
+                result = Add(result, multipy + i);
+                i += "0";
+            }
+
+            return Trim(result);
+        }
+
+        private string Trim(string number)
+        {
+            var result = "";
+            var begin = true;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (begin && number[i] == '0') continue;
+
+                begin = false;
+                result += number[i];
+            }
+
+            return result.Length > 0 ? result : "0";
+        }
+
+        private string Myltiply(string number, char x)
+        {
+            if (x == '0') return "0";
+
+            string result = "";
+            var i ="";
+
+            for (int j = number.Length-1; j >= 0; j--)
+            {
+                result = Add(result, Multipy(x, number[j]) + i);
+                i += "0";
+            }
+
+            return result;
+        }
+
+        private string Add(string x, string y)
+        {
+            var result = "";
+            var offset = 0;
+
+            for (int i = 0; i < Math.Max(x.Length, y.Length); i++)
+            {
+                var xIndex = x.Length - i;
+                var xi = xIndex > 0 ? x[xIndex-1] : '0';
+
+                var yIndex = y.Length - i;
+                var yi = yIndex > 0 ? y[yIndex-1] : '0';
+
+                var sum = xi + yi - 96 + offset;
+
+                result = (sum % 10) + result;
+                offset = sum / 10;
+            }
+
+            if (offset > 0) result = offset + result;
+
+            return result;
+        }
+
+        private int Multipy(char x, char y) => (x - 48) * (y - 48);
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/MultiplyStrings.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class MultiplyStrings
+    {
+        private LeetCode.Medium.MultiplyStrings Strings => new LeetCode.Medium.MultiplyStrings();
+
+        [TestMethod]
+        public void MultiplyStrings_2_3() =>
+            Assert.AreEqual("6", Strings.Multiply("2", "3"));
+
+        [TestMethod]
+        public void MultiplyStrings_123_456() =>
+            Assert.AreEqual("56088", Strings.Multiply("123", "456"));
+
+        [TestMethod]
+        public void MultiplyStrings_123456789_987654321() =>
+            Assert.AreEqual("121932631112635269", Strings.Multiply("123456789", "987654321"));
+
+        [TestMethod]
+        public void MultiplyStrings_123456789_0() =>
+            Assert.AreEqual("0", Strings.Multiply("123456789", "0"));
+
+        [Timeout(15)]
+        [TestMethod]
+        public void MultiplyStrings_398952156347820415337010868272413429836462364637363164372310573115219077216899627() =>
+            Assert.AreEqual("22227597896747931974032471411465102311462678900848060110229065542539766699641722110846910736641448270255228166899577189463",
+                Strings.Multiply(
+                "55714946123437258158091870754660433857669",
+                "398952156347820415337010868272413429836462364637363164372310573115219077216899627"));
+
+        [Timeout(10)]
+        [TestMethod]
+        public void MultiplyStrings_9369162965141127216164882458728854782080715827760307787224298083754() =>
+            Assert.AreEqual("67500649338953014190022427720009974152122888684303491539825680400401937831201855940252",
+                Strings.Multiply(
+                "9369162965141127216164882458728854782080715827760307787224298083754",
+                "7204554941577564438"));
+    }
+}
