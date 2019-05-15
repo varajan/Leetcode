7ac1605c04c45ad6e49f40c0021792b710076891
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-05-15 10:01:59 +0300

    #24 #23 LengthOfLastWord; CountSegments

---
+++ LeetCode/LeetCode/Easy/LastWordLength.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace LeetCode.Easy
+{
+    class LastWordLength
+    {
+        public int LengthOfLastWord(string s) => s.Trim().Split(' ').Last().Length;
+    }
+}
---
+++ LeetCode/LeetCode/Easy/NumberOfSegmentsInString.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Easy
+{
+    class NumberOfSegmentsInString
+    {
+        public int CountSegments(string s)
+        {
+            if (s.Trim() == "") return 0;
+
+            while (s.Contains("  ")) s = s.Replace("  ", " ");
+
+            return s.Trim().Split(' ').Length;
+        }
+    }
+}
