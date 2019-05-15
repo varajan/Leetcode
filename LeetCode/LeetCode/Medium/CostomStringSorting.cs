62553abe3525d260b692c0d4fd80e7c99c5794d6
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-05-15 11:01:18 +0300

    #25 Custom Sort String

---
+++ LeetCode/LeetCode/Medium/CostomStringSorting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Medium
+{
+    public class CostomStringSorting
+    {
+        public string CustomSortString(string S, string T)
+        {
+            var chars = T.ToList();
+
+            chars.Sort(new Comparer(S));
+
+            return chars.Aggregate(string.Empty, (current, x) => current += x); ;
+        }
+
+        class Comparer : IComparer<char>
+        {
+            private string _sort;
+
+            public Comparer(string sort)
+            {
+                _sort = sort;
+            }
+
+            int IComparer<char>.Compare(char x, char y)
+            {
+                var indexA = _sort.IndexOf(x);
+                var indexB = _sort.IndexOf(y);
+
+                if (indexA > 0 || indexB > 0)
+                {
+                    return indexA < indexB ? -1 : 1;
+                }
+
+                return x < y ? -1 : 1;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/CostomStringSorting.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class CostomStringSorting
+    {
+        private LeetCode.Medium.CostomStringSorting Sorting => new LeetCode.Medium.CostomStringSorting();
+
+        [TestMethod]
+        public void CostomStringSorting_01() => Assert.AreEqual("cdba", Sorting.CustomSortString("cba", "abcd"));
+
+        [TestMethod]
+        public void CostomStringSorting_02() => Assert.AreEqual("eevxw", Sorting.CustomSortString("exw", "xwvee"));
+    }
+}
