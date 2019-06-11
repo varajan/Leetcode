9968b4fb9ef336279da769610b2574f6be03071e
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-06-11 16:39:41 +0300

    #31 Solution with some stupid hack

---
+++ LeetCode/LeetCode/Hard/FindShortestPalindrome.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace LeetCode.Hard
+{
+    public class FindShortestPalindrome
+    {
+        public string ShortestPalindrome(string s)
+        {
+            int len = s.Length;
+            int step = len < 10000 ? 1 : 73;
+
+            for (var i = len; i > 0; i-=step)
+            {
+                if (IsPalindrome(ref s, i))
+                {
+                    var tail = s.Substring(i, len - i);
+                    var head = tail.Aggregate("", (current, t) => t + current);
+
+                    return head + s;
+                }
+            }
+
+            return "";
+        }
+
+        private bool IsPalindrome(ref string line, int length)
+        {
+            for (var i = 0; i < length / 2; i++)
+                if (line[i] != line[length - i - 1])
+                    return false;
+
+            return true;
+        }
+    }
+}
