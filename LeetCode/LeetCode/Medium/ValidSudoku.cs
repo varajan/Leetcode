e10c7843fb13d1b44a24952aa9160c152141af43
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-01 15:02:55 +0300

    #36 ValidSudoku

---
+++ LeetCode/LeetCode/Medium/ValidSudoku.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Medium
+{
+    public class ValidSudoku
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (!IsRowValid(i))    return false;
+                if (!IsColumnValid(i)) return false;
+                if (!IsRectValid(i))   return false;
+            }
+
+            return true;
+
+            bool IsRowValid(int row)
+            {
+                var chars = new HashSet<int>();
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if (board[row][i] == '.') continue;
+                    if (chars.Contains(board[row][i])) return false;
+
+                    chars.Add(board[row][i]);
+                }
+
+                return true;
+            }
+
+            bool IsColumnValid(int column)
+            {
+                var chars = new HashSet<int>();
+
+                for (int i = 0; i < 9; i++)
+                {
+                    if (board[i][column] == '.') continue;
+                    if (chars.Contains(board[i][column])) return false;
+
+                    chars.Add(board[i][column]);
+                }
+
+                return true;
+            }
+
+            bool IsRectValid(int rectangle)
+            {
+                // 0 1 2
+                // 3 4 5
+                // 6 7 8
+
+                var chars = new HashSet<int>();
+
+                for (int i = 0; i < 9; i++)
+                {
+                    int x = getX(i);
+                    int y = getY(i);
+
+                    if (board[x][y] == '.') continue;
+                    if (chars.Contains(board[x][y])) return false;
+
+                    chars.Add(board[x][y]);
+                }
+
+                return true;
+
+                int getX(int i) => (rectangle / 3)*3 + i / 3;
+
+                int getY(int i) => (rectangle % 3)*3 + i % 3;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/ValidSudoku.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class ValidSudoku
+    {
+        private static LeetCode.Medium.ValidSudoku Sudoku => new LeetCode.Medium.ValidSudoku();
+
+        [TestMethod]
+        public void ValidSudoku_01()
+        {
+            var board = new[]
+            {
+                new[] {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                new[] {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                new[] {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                new[] {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                new[] {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                new[] {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                new[] {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                new[] {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                new[] {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+            };
+
+            Assert.IsTrue(Sudoku.IsValidSudoku(board));
+        }
+
+        [TestMethod]
+        public void ValidSudoku_02()
+        {
+            var board = new[] {
+                new[] {'8','3','.','.','7','.','.','.','.'},
+                new[] {'6','.','.','1','9','5','.','.','.'},
+                new[] {'.','9','8','.','.','.','.','6','.'},
+                new[] {'8','.','.','.','6','.','.','.','3'},
+                new[] {'4','.','.','8','.','3','.','.','1'},
+                new[] {'7','.','.','.','2','.','.','.','6'},
+                new[] {'.','6','.','.','.','.','2','8','.'},
+                new[] {'.','.','.','4','1','9','.','.','5'},
+                new[] {'.','.','.','.','8','.','.','7','9'}
+            };
+
+            Assert.IsFalse(Sudoku.IsValidSudoku(board));
+        }
+    }
+}
