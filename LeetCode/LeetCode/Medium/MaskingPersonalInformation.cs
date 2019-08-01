4e440207a99dc400c2d7cc319e2b7e46a281418f
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-01 12:23:20 +0300

    #35 MaskingPersonalInformation

---
+++ LeetCode/LeetCode/Medium/MaskingPersonalInformation.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+
+namespace LeetCode.Medium
+{
+    public class MaskingPersonalInformation
+    {
+        public string MaskPII(string S)
+        {
+            return S.Contains('@') ? GetEmailAnonymized() : GetPhoneAnonymized();
+
+            string GetEmailAnonymized()
+            {
+                S = S.ToLower();
+                var first = S.First();
+                var last = S.Split('@')[0].Last();
+                var domain = S.Split('@').Last();
+
+                return $"{first}*****{last}@{domain}";
+            }
+
+            string GetPhoneAnonymized()
+            {
+                var phone = "";
+                var result = "";
+
+                foreach (var c in S)
+                {
+                    if (char.IsDigit(c)) phone += $"{c}";
+                }
+
+                for (int i = 0; i < phone.Length; i++)
+                {
+                    result += i < phone.Length - 4 ? "*" : phone[i].ToString();
+                }
+
+                if (result.Length > 10) result = "+" + result;
+
+                for (var i = 1; i < 4 && result.Length - 4 * i > 0; i++)
+                {
+                    result = result.Insert(result.Length - 4 * i, "-");
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/MaskingPersonalInformation.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class MaskingPersonalInformation
+    {
+        private static LeetCode.Medium.MaskingPersonalInformation Masking =>
+            new LeetCode.Medium.MaskingPersonalInformation();
+
+        [TestMethod]
+        public void MaskingPersonalInformation_01()
+        {
+            Assert.AreEqual("l*****e@leetcode.com", Masking.MaskPII("LeetCode@LeetCode.com"));
+        }
+
+        [TestMethod]
+        public void MaskingPersonalInformation_02()
+        {
+            Assert.AreEqual("a*****b@qq.com", Masking.MaskPII("AB@qq.com"));
+        }
+
+        [TestMethod]
+        public void MaskingPersonalInformation_03()
+        {
+            Assert.AreEqual("***-***-7890", Masking.MaskPII("1(234)567-890"));
+        }
+
+        [TestMethod]
+        public void MaskingPersonalInformation_04()
+        {
+            Assert.AreEqual("+**-***-***-5678", Masking.MaskPII("86-(10)12345678"));
+        }
+
+        [TestMethod]
+        public void MaskingPersonalInformation_05()
+        {
+            Assert.AreEqual("+***-***-***-3431", Masking.MaskPII("+(501321)-50-23431"));
+        }
+    }
+}
