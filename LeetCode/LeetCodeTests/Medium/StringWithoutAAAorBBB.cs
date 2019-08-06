bdbe71d195653134bcf0842e771f11d3ec486b11
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-06 11:07:21 +0300

    #37 StrWithout3a3b

---
+++ LeetCode/LeetCodeTests/Medium/StringWithoutAAAorBBB.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeTests.Medium
+{
+    class StringWithoutAAAorBBB
+    {
+        public string StrWithout3a3b(int A, int B)
+        {
+            string result = "";
+
+            while (A > 0 || B > 0)
+            {
+                if (A > B && !result.EndsWith("aa") || result.EndsWith("bb"))
+                {
+                    A--;
+                    result += "a";
+                }
+                else
+                {
+                    B--;
+                    result += "b";
+                }
+            }
+
+            return result;
+        }
+    }
+}
