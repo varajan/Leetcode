6f974259d83c9cda51ba6fc8e0df77822a937302
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-06 13:01:26 +0300

    #39 Jump Game

---
+++ LeetCode/LeetCode/Medium/JumpGame.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Medium
+{
+    public class JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            var visited = new bool[nums.Length];
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    visited.Add(false);
+            //}
+
+            return TryJump(0);
+
+            bool TryJump(int i)
+            {
+                SetVisited(i);
+
+                if (i + 1 >= nums.Length) return true;
+
+                for (int x = nums[i]; x > 0; x--)
+                {
+                    if (IsVisited(i+x)) continue;
+
+                    var jump = TryJump(i + x);
+                    if (jump) return true;
+                }
+
+                return false;
+            }
+
+            bool IsVisited(int i) => i < visited.Length && visited[i];
+            void SetVisited(int i)
+            {
+                if (i < visited.Length) visited[i] = true;
+            }
+        }
+    }
+}
