ad6efa1564af1245555bc5712211039367e61013
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-07 14:56:13 +0300

    #41 Car Fleet

---
+++ LeetCode/LeetCode/Medium/Car_Fleet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Medium
+{
+    public class Car_Fleet
+    {
+        public int CarFleet(int target, int[] position, int[] speed)
+        {
+            int result = 0;
+
+            var cars = speed
+                .Select((x, i) => (position: position[i], speed: speed[i]))
+                .OrderByDescending(car => car.position).ToList();
+
+            while (cars.Any())
+            {
+                cars = cars.Select(car => (position: car.position + car.speed, car.speed)).ToList();
+
+                var previous = cars.First();
+                var fleet = new List<(int position, int speed)> { previous };
+
+                for (var i = 1; i < cars.Count; i++)
+                {
+                    if (AreSmashed(cars[i], previous)) continue;
+
+                    fleet.Add(cars[i]);
+                    previous = cars[i];
+                }
+
+                result += fleet.Count(car => car.position >= target);
+
+                cars = fleet.Where(car => car.position < target).ToList();
+
+            }
+
+            return result;
+
+            bool AreSmashed((int position, int speed) current, (int position, int speed) previous) =>
+                previous.position <= current.position && ArrivalTime(previous) <= ArrivalTime(current);
+
+            double ArrivalTime((int position, int speed) car) => ((double)car.position - target) / car.speed;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/Car_Fleet.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class Car_Fleet
+    {
+        private static LeetCode.Medium.Car_Fleet Fleet => new LeetCode.Medium.Car_Fleet();
+
+        [TestMethod]
+        public void Car_Fleet_01()
+        {
+            var target = 12;
+            var position = new[] {10, 8, 0, 5, 3};
+            var speed = new[] {2, 4, 1, 1, 3};
+
+            Assert.AreEqual(3, Fleet.CarFleet(target, position, speed));
+        }
+
+        [TestMethod]
+        public void Car_Fleet_02()
+        {
+            var target = 16;
+            var position = new[] {11, 14, 13, 6};
+            var speed = new[] {2, 2, 6, 7};
+
+            Assert.AreEqual(2, Fleet.CarFleet(target, position, speed));
+        }
+
+        [TestMethod]
+        public void Car_Fleet_03()
+        {
+            var target = 10;
+            var position = new[] {6, 8};
+            var speed = new[] {3, 2};
+
+            Assert.AreEqual(2, Fleet.CarFleet(target, position, speed));
+        }
+
+        [TestMethod]
+        public void Car_Fleet_04()
+        {
+            var target = 13;
+            var position = new[] {10, 2, 5, 7, 4, 6, 11};
+            var speed = new[] {7, 5, 10, 5, 9, 4, 1};
+
+            Assert.AreEqual(2, Fleet.CarFleet(target, position, speed));
+        }
+
+        [TestMethod]
+        public void Car_Fleet_05()
+        {
+            var target = 31;
+            var position = new[] {5, 26, 18, 25, 29, 21, 22, 12, 19, 6};
+            var speed = new[] {7, 6, 6, 4, 3, 4, 9, 7, 6, 4};
+
+            Assert.AreEqual(6, Fleet.CarFleet(target, position, speed));
+        }
+    }
+}
