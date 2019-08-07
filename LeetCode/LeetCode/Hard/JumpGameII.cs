e1554a31b779dce87f6810f7916d9e7b75320b6e
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-07 08:11:46 +0300

    JumpGameII

---
+++ LeetCode/LeetCode/Hard/JumpGameII.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace LeetCode.Hard
+{
+    public class JumpGameII
+    {
+        public int Jump(int[] nums)
+        {
+            var size = nums.Length;
+            var visited = new int[size];
+            int result = int.MaxValue;
+            Jump(0);
+
+            return result;
+
+            void Jump(int i, int step = 0)
+            {
+                if (size <= i + 1)
+                {
+                    result = Math.Min(result, step);
+                    return;
+                }
+
+                SetVisited(i, step++);
+
+                for (int x = nums[i]; x > 0; x--)
+                {
+                    //Jump(i + x, step);
+                    if (ValidToVisit(i + x, step)) Jump(i + x, step);
+                }
+            }
+
+            bool ValidToVisit(int i, int steps)
+            {
+                return i >= size || visited[i] == 0 || visited[i] >= steps;
+            }
+
+            void SetVisited(int i, int steps)
+            {
+                visited[i] = steps;
+            }
+        }
+    }
+}
