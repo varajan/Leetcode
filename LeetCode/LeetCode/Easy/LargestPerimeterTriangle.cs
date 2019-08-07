2dcf9e47d5b689be1fb4bebaeeac96dd3b346e22
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-07 15:54:08 +0300

    #42 Largest Perimeter Triangle

---
+++ LeetCode/LeetCode/Easy/LargestPerimeterTriangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Easy
+{
+    class LargestPerimeterTriangle
+    {
+        public int LargestPerimeter(int[] A)
+        {
+            Array.Sort(A);
+
+            int a, b, c;
+
+            for (int i = A.Length - 1; i > 1; i--)
+            {
+                a = A[i];
+                b = A[i - 1];
+                c = A[i - 2];
+
+                if (b + c > a) return a + b + c;
+            }
+
+            return 0;
+        }
+    }
+}
