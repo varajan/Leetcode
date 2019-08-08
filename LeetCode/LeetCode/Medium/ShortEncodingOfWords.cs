a145bfa61ccf5a9865686a643830d2fcbf15d59d
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-08 16:38:43 +0300

    #45 Short Encoding of Words

---
+++ LeetCode/LeetCode/Medium/ShortEncodingOfWords.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace LeetCode.Medium
+{
+    public class ShortEncodingOfWords
+    {
+        public int MinimumLengthEncoding(params string[] words)
+        {
+            var previous = "";
+            var result = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = Reverse(words[i]);
+            }
+
+            Array.Sort(words);
+
+            for (int i = words.Length-1; i >= 0; i--)
+            {
+                var word = words[i];
+                if (previous.StartsWith(word)) continue;
+
+                result += word.Length + 1;
+                previous = word;
+            }
+
+            return result;
+
+            string Reverse(string word)
+            {
+                var reverse = new StringBuilder();
+
+                for (int i = word.Length - 1; i >= 0; i--) reverse.Append(word[i]);
+
+                return reverse.ToString();
+            }
+        }
+    }
+}
