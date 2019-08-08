48d74d15a7c3408fae0ef275acf202283c617576
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-08 14:22:12 +0300

    #44 Count Numbers with Unique Digits

---
+++ LeetCode/LeetCode/Medium/CountNumbersWithUniqueDigits.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Medium
+{
+    public class CountNumbersWithUniqueDigits
+    {
+        public int Count(int n)
+        {
+            int result = 1;
+
+            for (int i = 10; i > 10-n; i--) result *= i;
+
+            for (int i = 1; i <= n - 1; i++)
+            {
+                var x = 1;
+                for (int j = 9; j > 10-i; j--) x *= j;
+
+                result += x * i;
+            }
+
+            return result;
+        }
+    }
+}
+
+//74
---
+++ LeetCode/LeetCodeTests/Medium/CountNumbersWithUniqueDigits.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class CountNumbersWithUniqueDigits
+    {
+        private static LeetCode.Medium.CountNumbersWithUniqueDigits Digits => new LeetCode.Medium.CountNumbersWithUniqueDigits();
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_01() => Assert.AreEqual(10, Digits.Count(1));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_02() => Assert.AreEqual(91, Digits.Count(2));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_03() => Assert.AreEqual(739, Digits.Count(3));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_04() => Assert.AreEqual(5_275, Digits.Count(4));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_05() => Assert.AreEqual(32_491, Digits.Count(5));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_06() => Assert.AreEqual(168_571, Digits.Count(6));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_07() => Assert.AreEqual(712_891, Digits.Count(7));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_08() => Assert.AreEqual(2_345_851, Digits.Count(8));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_09() => Assert.AreEqual(5_611_771, Digits.Count(9));
+
+        [Timeout(500)]
+        [TestMethod]
+        public void CountNumbersWithUniqueDigits_10() => Assert.AreEqual(8_877_691, Digits.Count(10));
+    }
+}
