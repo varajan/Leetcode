498432f6451ac525223ab617dd51ac7efc6870e4
Sergii Dmytrenko <s.dmytrenko@levi9.com>
2019-08-09 11:37:05 +0300

    # Unique Email Addresses

---
+++ LeetCode/LeetCode/Easy/UniqueEmailAddresses.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace LeetCode.Easy
+{
+    class UniqueEmailAddresses
+    {
+        public int NumUniqueEmails(string[] emails)
+        {
+            return emails.Select(Normalized).Distinct().Count();
+
+            string Normalized(string mail)
+            {
+                var parts = mail.Split('@');
+
+                return parts[0].Split('+')[0].Replace(".", "") + "@" + parts[1];
+            }
+        }
+    }
+}
