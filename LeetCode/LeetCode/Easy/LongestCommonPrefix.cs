a036556faa41b664a214505775d73119717b20f5
Sergii <30838113+varajan@users.noreply.github.com>
2019-08-21 14:16:04 +0300

    Create LongestCommonPrefix.cs

---
+++ LeetCode/LeetCode/Easy/LongestCommonPrefix.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        var result = "";
+        var word = strs.Any() ? strs[0] : string.Empty;
+        
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (!strs.All(x => x.StartsWith(result + word[i])))
+                break;
+           
+            result += word[i];
+        }
+        
+        return result;
+    }
+}
