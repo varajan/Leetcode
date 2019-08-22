571f904284e98ee9b2ecf61fd0e0e368e992f961
Sergii <30838113+varajan@users.noreply.github.com>
2019-08-22 12:39:45 +0300

    Create ValidPalindrome.cs

---
+++ LeetCode/LeetCode/Easy/ValidPalindrome.cs
@@ -0,0 +1,20 @@
+public class ValidPalindrome {
+    public bool IsPalindrome(string s) {
+        if (s.Length == 0) return true;
+
+        int i = -1;
+        int j = s.Length;
+
+        while (i < j)
+        {
+            while (i < s.Length - 1 && !isAlphaNumeric(s[++i]));
+            while (j > 0            && !isAlphaNumeric(s[--j]));
+            
+            if (i < j && Char.ToLower(s[i]) != Char.ToLower(s[j])) return false;
+        }
+
+        return true;
+    }
+    
+    private bool isAlphaNumeric(char x) => Char.IsDigit(x) || Char.IsLetter(x);
+}
