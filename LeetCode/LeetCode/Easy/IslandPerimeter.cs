96728373f07754c98cc40fa1714dd0efdf18fa29
Sergii <30838113+varajan@users.noreply.github.com>
2019-08-23 18:20:50 +0300

    Create IslandPerimeter.cs

---
+++ LeetCode/LeetCode/Easy/IslandPerimeter.cs
@@ -0,0 +1,24 @@
+public class IslandPerimeter {
+    public int IslandPerimeter(int[][] grid) {
+        int result = 0;
+        
+        for (int x = 0; x < grid.Length; x++)
+            for (int y = 0; y < grid[0].Length; y++)
+                if (grid[x][y] == 1)
+                    result += GetBorders(x, y);
+        
+        return result;
+        
+        int GetBorders(int x, int y)
+        {
+            int neighborhoods = 0;
+            
+            if (x > 0 && grid[x-1][y] == 1) neighborhoods++;
+            if (y > 0 && grid[x][y-1] == 1) neighborhoods++;
+            if (x < grid.Length - 1 && grid[x + 1][y] == 1) neighborhoods++;
+            if (y < grid[0].Length - 1 && grid[x][y + 1] == 1) neighborhoods++;
+            
+            return 4 - neighborhoods;
+        }
+    }
+}
