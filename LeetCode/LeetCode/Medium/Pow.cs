f918c75dee3a6b6d71793626f389ff9ebfe87fd6
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-08-27 18:08:27 +0300

    #62 Pow

---
+++ LeetCode/LeetCode/Medium/Pow.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Medium
+{
+    public class Pow
+    {
+        public double MyPow(double x, int n)
+        {
+            if (x == 0 || x == 1) return x;
+            if (x == -1) n = n % 2;
+
+            double result = 1;
+            bool positive = n > 0;
+
+            for (int i = positive ? 0 : n; i < (positive ? n : 0) && result != 0; i++)
+                if (positive)
+                    result *= x;
+                else
+                    result /= x;
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Medium/Pow.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Medium
+{
+    [TestClass]
+    public class Pow
+    {
+        private static LeetCode.Medium.Pow PowXN => new LeetCode.Medium.Pow();
+
+        [TestMethod]
+        public void Pow_01() => Assert.AreEqual(1024, PowXN.MyPow(2, 10));
+
+        [TestMethod]
+        public void Pow_02() => Assert.AreEqual(9.261000000000001, PowXN.MyPow(2.1, 3));
+
+        [TestMethod]
+        public void Pow_03() => Assert.AreEqual(0.25, PowXN.MyPow(2, -2));
+
+        [Timeout(1000)]
+        [TestMethod]
+        public void Pow_04() => Assert.AreEqual(0, PowXN.MyPow(0.00001, 2147483647));
+
+        [Timeout(1000)]
+        [TestMethod]
+        public void Pow_05() => Assert.AreEqual(1, PowXN.MyPow(1, 2147483647));
+
+        [Timeout(1000)]
+        [TestMethod]
+        public void Pow_06() => Assert.AreEqual(0, PowXN.MyPow(2, -2147483648));
+
+        [Timeout(1000)]
+        [TestMethod]
+        public void Pow_07() => Assert.AreEqual(-1, PowXN.MyPow(-1, 2147483647));
+    }
+}
