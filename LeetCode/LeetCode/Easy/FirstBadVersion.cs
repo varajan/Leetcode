35cb25f111d3562887d2b05f37015c1763313ffc
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-08-28 10:02:05 +0300

    #63 First Bad Version

---
+++ LeetCode/LeetCode/Easy/FirstBadVersion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Easy
+{
+    public class FirstBadVersion
+    {
+        IVersionControl _versionControl;
+
+        public FirstBadVersion(IVersionControl versionControl)
+        {
+            _versionControl = versionControl;
+        }
+
+        public int GetFirstBadVersion(int n)
+        {
+            var versions = new Dictionary<int, bool>();
+            double min = 1;
+            double max = n;
+
+            while (min != max)
+            {
+                int version = (int)(max / 2 + min / 2);
+
+                if (!versions.ContainsKey(version)) versions.Add(version, _versionControl.IsBadVersion(version));
+
+                if (max == version || min == version) return versions[version] ? version : version + 1;
+
+                max = versions[version] ? version : max;
+                min = !versions[version] ? version : min;
+            }
+
+            return (int)max;
+        }
+    }
+
+    public interface IVersionControl
+    {
+        bool IsBadVersion(int version);
+    }
+}
---
+++ LeetCode/LeetCodeTests/Easy/FirstBadVersion.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace LeetCodeTests.Easy
+{
+    [TestClass]
+    public class FirstBadVersion
+    {
+        private void Test(int n, int version)
+        {
+            var moq = new Mock<IVersionControl>();
+            moq.Setup(x => x.IsBadVersion(It.IsAny<int>())).Returns<int>(x => x >= version);
+            var versionControl = new LeetCode.Easy.FirstBadVersion(moq.Object);
+
+            Assert.AreEqual(version, versionControl.GetFirstBadVersion(n));
+        }
+
+        [TestMethod]
+        public void FirstBadVersion_01() => Test(2, 1);
+
+        [TestMethod]
+        public void FirstBadVersion_02() => Test(5, 4);
+
+        [TestMethod]
+        public void FirstBadVersion_03() => Test(3, 3);
+
+        [TestMethod]
+        public void FirstBadVersion_04() => Test(2126753390, 1702766719);
+    }
+}
