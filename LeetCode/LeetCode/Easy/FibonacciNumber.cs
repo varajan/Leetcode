682fffb7157107ce3c6faeb7a71291f56def2226
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-08-28 10:32:47 +0300

    #65 FibonacciNumber

---
+++ LeetCode/LeetCode/Easy/FibonacciNumber.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Easy
+{
+    class FibonacciNumber
+    {
+        public int Fib(int N)
+        {
+            if (N == 0) return 0;
+            if (N == 1) return 1;
+            if (N == 2) return 1;
+
+            return 2 * Fib(N - 2) + Fib(N - 3);
+        }
+    }
+}
