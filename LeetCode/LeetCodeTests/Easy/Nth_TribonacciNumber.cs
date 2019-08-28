6a22a44e414c1da11e521cdd7e84e90d3b827f74
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-08-28 11:01:39 +0300

    #66 add 'timeout' to all unit tests

--- LeetCode/LeetCodeTests/Easy/Nth_TribonacciNumber.cs
+++ LeetCode/LeetCodeTests/Easy/Nth_TribonacciNumber.cs
@@ -7,23 +7,28 @@ namespace LeetCodeTests.Easy
     {
         private static LeetCode.Easy.Nth_TribonacciNumber Tribonacci => new LeetCode.Easy.Nth_TribonacciNumber();
 
-        [Timeout(1000)]
-        [TestMethod]
-        public void Nth_TribonacciNumber_of_33_is_181997601() => Assert.AreEqual(181997601, Tribonacci.Tribonacci(33));
-
+        [Timeout(10)]
         [TestMethod]
         public void Nth_TribonacciNumber_of_0_is_0() => Assert.AreEqual(0, Tribonacci.Tribonacci(0));
 
+        [Timeout(10)]
         [TestMethod]
         public void Nth_TribonacciNumber_of_1_is_1() => Assert.AreEqual(1, Tribonacci.Tribonacci(1));
 
+        [Timeout(10)]
         [TestMethod]
         public void Nth_TribonacciNumber_of_2_is_1() => Assert.AreEqual(1, Tribonacci.Tribonacci(2));
 
+        [Timeout(10)]
         [TestMethod]
         public void Nth_TribonacciNumber_of_3_is_2() => Assert.AreEqual(2, Tribonacci.Tribonacci(3));
 
+        [Timeout(10)]
         [TestMethod]
         public void Nth_TribonacciNumber_of_4_is_4() => Assert.AreEqual(4, Tribonacci.Tribonacci(4));
+
+        [Timeout(10)]
+        [TestMethod]
+        public void Nth_TribonacciNumber_of_33_is_181997601() => Assert.AreEqual(181997601, Tribonacci.Tribonacci(33));
     }
 }
