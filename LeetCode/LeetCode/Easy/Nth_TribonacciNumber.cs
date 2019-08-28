d0c8d314056123b6da26987b088fe059b45c7382
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-08-28 11:00:23 +0300

    #66 N-th Tribonacci Number

---
+++ LeetCode/LeetCode/Easy/Nth_TribonacciNumber.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Easy
+{
+    public class Nth_TribonacciNumber
+    {
+        public int Tribonacci(int n)
+        {
+            var tribonacci = new List<int> { 0, 1, 1 };
+
+            for (int i = 3; i <= n; i++)
+            {
+                tribonacci.Add(tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3]);
+            }
+
+            return tribonacci[n];
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Easy/Nth_TribonacciNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCodeTests.Easy
+{
+    [TestClass]
+    public class Nth_TribonacciNumber
+    {
+        private static LeetCode.Easy.Nth_TribonacciNumber Tribonacci => new LeetCode.Easy.Nth_TribonacciNumber();
+
+        [Timeout(1000)]
+        [TestMethod]
+        public void Nth_TribonacciNumber_of_33_is_181997601() => Assert.AreEqual(181997601, Tribonacci.Tribonacci(33));
+
+        [TestMethod]
+        public void Nth_TribonacciNumber_of_0_is_0() => Assert.AreEqual(0, Tribonacci.Tribonacci(0));
+
+        [TestMethod]
+        public void Nth_TribonacciNumber_of_1_is_1() => Assert.AreEqual(1, Tribonacci.Tribonacci(1));
+
+        [TestMethod]
+        public void Nth_TribonacciNumber_of_2_is_1() => Assert.AreEqual(1, Tribonacci.Tribonacci(2));
+
+        [TestMethod]
+        public void Nth_TribonacciNumber_of_3_is_2() => Assert.AreEqual(2, Tribonacci.Tribonacci(3));
+
+        [TestMethod]
+        public void Nth_TribonacciNumber_of_4_is_4() => Assert.AreEqual(4, Tribonacci.Tribonacci(4));
+    }
+}
