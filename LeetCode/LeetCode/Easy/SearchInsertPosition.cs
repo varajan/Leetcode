fe8974297007881f377c29c2596224c15294d890
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-08-28 10:10:15 +0300

    #64  Search Insert Position

---
+++ LeetCode/LeetCode/Easy/SearchInsertPosition.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Easy
+{
+    class SearchInsertPosition
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] >= target) return i;
+            }
+
+            return nums.Length;
+        }
+    }
+}
