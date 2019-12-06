7d57f9c7851513baf4c58c1b13a7eb2e48d9f1d3
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-12-06 10:27:07 +0200

    #69 Remove Element

---
+++ LeetCode/LeetCode/Easy/RemoveElements.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Easy
+{
+    public class RemoveElements
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            int length = nums.Length;
+
+            for(var i = 0; i < length; i++)
+            {
+                if (nums[i] == val)
+                {
+                    while(i < length)
+                    {
+                        if (nums[length - 1] != val) break;
+
+                        length--;
+                    }
+
+                   if (length > i) nums[i] = nums[--length];
+                }
+            }
+
+            return length > 0 ? length : 0;
+        }
+    }
+}
