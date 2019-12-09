a8987d7cb6a09ea828c74be0a40431d7156b0607
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-12-09 16:54:23 +0200

    #71 Compress

---
+++ LeetCode/LeetCode/Easy/CompressSolution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Easy
+{
+    public class CompressSolution
+    {
+        public int Compress(char[] chars)
+        {
+            int result = 0;
+            var count = 1;
+            var symbol = chars[0];
+
+            for(var i = 1; i < chars.Length; i++)
+            {
+                if (chars[i] == symbol) { count++; }
+                else
+                {
+                    AdustArray();
+
+                    count = 1;
+                    symbol = chars[i];
+                }
+            }
+
+            AdustArray();
+
+            return result;
+
+            // ---------------------------------------------------------
+            void AdustArray()
+            {
+                chars[result] = symbol;
+                result++;
+
+                if (count > 1)
+                {
+                    if (count > 9) { chars[result++] = (char)(count / 10 + 48); count %= 10; }
+                    chars[result++] = (char)(count + 48);
+                }
+            }
+            // ---------------------------------------------------------
+        }
+    }
+}
