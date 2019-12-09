775d609a2a6ea52c42fd912474701f326fb18666
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-12-09 15:48:57 +0200

    #70 Count and say

---
+++ LeetCode/LeetCode/Easy/CountAndSaySolution.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+
+namespace LeetCode.Easy
+{
+    public class CountAndSaySolution
+    {
+        public string CountAndSay(int n)
+        {
+            var result = "1";
+
+            for (var i = 1; i < n; i++)
+            {
+                result = CountAndSayR(result);
+            }
+
+            return result;
+        }
+
+        private string  CountAndSayR(string sequence)
+        {
+            var result = "";
+            var count = 1;
+            var symbol = sequence.First();
+
+            for(var x = 1; x < sequence.Length; x++)
+            {
+                if (sequence[x] == symbol)
+                {
+                    count++;
+                }
+                else
+                {
+                    result += $"{count}{symbol}";
+                    count = 1;
+                    symbol = sequence[x];
+                }
+            }
+
+            result += $"{count}{symbol}";
+
+            return result;
+        }
+    }
+}
