b62d5452ff953954782eed096b432dce86faf50e
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-12-10 10:06:54 +0200

    #73 Add binary

---
+++ LeetCode/LeetCode/Easy/AddBinarySolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Easy
+{
+    public class AddBinarySolution
+    {
+        public string AddBinary(string a, string b)
+        {
+            string result = "";
+            char offset = '0';
+
+            for (int i = 0; i < Math.Max(a.Length, b.Length); i++)
+            {
+                var codes = new List<char>
+                {
+                    (a.Length - i > 0) ? a[a.Length - i -1] : '0',
+                    (b.Length - i > 0) ? b[b.Length - i -1] : '0',
+                    offset
+                };
+
+                var ones = codes.Where(c => c == '1').Count();
+                offset = ones > 1 ? '1' : '0';
+
+                result = (ones == 1 || ones == 3 ? '1' : '0') + result;
+            }
+
+            result = offset + result;
+
+            while (result.Length> 1 && result[0] == '0')
+            {
+                result = result.Remove(0, 1);
+            }
+
+            return result;
+        }
+    }
+}
--- LeetCode/LeetCode/Easy/PlusOneSolution.cs
+++ LeetCode/LeetCode/Easy/PlusOneSolution.cs
@@ -4,28 +4,27 @@ namespace LeetCode.Easy
 {
     public class PlusOneSolution
     {
-            public int[] PlusOne(int[] digits)
+        public int[] PlusOne(int[] digits)
+        {
+            var offset = 1;
+            var result = digits.ToList();
+
+            for (int i = result.Count - 1; i >= 0; i--)
             {
-                var offset = 1;
-                var result = digits.ToList();
-
-                for (int i = result.Count - 1; i >= 0; i--)
+                if (result[i]++ == 9)
                 {
-                    if (result[i]++ == 9)
-                    {
-                        result[i] = 0;
-                    }
-                    else
-                    {
-                        offset = 0;
-                        break;
-                    }
+                    result[i] = 0;
                 }
-
-                if (offset == 1) result.Insert(0, 1);
-
-                return result.ToArray();
+                else
+                {
+                    offset = 0;
+                    break;
+                }
             }
+
+            if (offset == 1) result.Insert(0, 1);
+
+            return result.ToArray();
         }
     }
 }
