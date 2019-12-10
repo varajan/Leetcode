3877d77e16a7cdc2ccea1d6e0a330337954a3361
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-12-10 09:22:34 +0200

    #72 Plus One

---
+++ LeetCode/LeetCode/Easy/PlusOneSolution.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+
+namespace LeetCode.Easy
+{
+    public class PlusOneSolution
+    {
+            public int[] PlusOne(int[] digits)
+            {
+                var offset = 1;
+                var result = digits.ToList();
+
+                for (int i = result.Count - 1; i >= 0; i--)
+                {
+                    if (result[i]++ == 9)
+                    {
+                        result[i] = 0;
+                    }
+                    else
+                    {
+                        offset = 0;
+                        break;
+                    }
+                }
+
+                if (offset == 1) result.Insert(0, 1);
+
+                return result.ToArray();
+            }
+        }
+    }
+}
