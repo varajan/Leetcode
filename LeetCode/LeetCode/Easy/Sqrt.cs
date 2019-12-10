db300755d0df17f5e5aecb0d6e6116d02d5b7fb2
Sergii Dmytrenko <30838113+varajan@users.noreply.github.com>
2019-12-10 10:12:54 +0200

    #74 Sqrt

---
+++ LeetCode/LeetCode/Easy/Sqrt.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode.Easy
+{
+    class Sqrt
+    {
+        public int MySqrt(int x) => int.Parse(Math.Sqrt(x).ToString().Split('.').First());
+    }
+}
